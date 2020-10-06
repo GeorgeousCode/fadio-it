001ece328eee0429f69fa5007d53bf4b85ce41b7
GeorgeousCode <jodye-clothing@hotmail.com>
2020-10-06 16:17:32 +0200

    Updates

---
+++ precache-manifest.d4760f492a6153595d099ecdd2584601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4240aee49e7f2d4922ba47ae8f214b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abd3aa01aba70eb59f58",
+    "url": "/static/css/main.11f82295.chunk.css"
+  },
+  {
+    "revision": "7c8f2c8c147c254a0f80",
+    "url": "/static/js/2.e80e1e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e80e1e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd3aa01aba70eb59f58",
+    "url": "/static/js/main.82d8cc65.chunk.js"
+  },
+  {
+    "revision": "623600666cfc29ff14b5",
+    "url": "/static/js/runtime-main.ee311ca8.js"
+  }
+]);
